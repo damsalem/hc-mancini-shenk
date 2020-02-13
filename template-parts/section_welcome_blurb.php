df03be8e86cac27f70bce32fab0d5eada446d1b7
damsalem <drummingdan@gmail.com>
2020-02-12 22:47:31 -0500

    Minor Tweaks to Header & Welcome Blurb Fixed the alt tag and title of header logo Removed padding from style.css of welcome blurb and added it as Bootstrap classes

--- template-parts/section_welcome_blurb.php
+++ template-parts/section_welcome_blurb.php
@@ -3,11 +3,11 @@
         <div class="container">
             <!-- <div class="row"> -->
                 <!-- <div class="col-12"> -->
-                    <div class="carousel-link welcome-links">
+                    <div class="carousel-link welcome-links d-none d-lg-block">
                         <ul class="contact-info">
-                            <li><p><strong>1925 Century Park East, Suite 1700, Los Angeles, CA 90067</strong></p></li>
-                            <li><p><strong>(424) 652-4009</strong></p></li>
-                            <li><p><strong>email@address.com</strong></p></li>
+                            <li class="px-lg-3 px-md-1"><p><strong>1925 Century Park East, Suite 1700, Los Angeles, CA 90067</strong></p></li>
+                            <li class="px-lg-3 px-md-1"><p><strong>(424) 652-4009</strong></p></li>
+                            <li class="px-lg-3 px-md-1"><p><strong>email@address.com</strong></p></li>
                         </ul>
                     </div>
                 <!-- </div> -->
